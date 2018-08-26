49f2b9540059bc1ca5796ad628ae3a1a69ea738c
JasonYZ <jasonzhang2013@outlook.com>
2018-08-26 16:38:33 +0100

    Move class methods from utils to SSPC

--- utils.py
+++ utils.py
@@ -1,11 +1,6 @@
 import numpy as np
 import pandas
-from sklearn import preprocessing
-
-
-k = 6    # Number of clusters
-m = 0.5  # Threshold coefficient, m varies between (0,1)
-building_dim_num = 3
+# from sklearn import preprocessing
 
 
 def preprocess(input_path):
@@ -25,12 +20,6 @@ def preprocess(input_path):
     # data = preprocessing.scale(np.array(df))
     data = np.array(df)
     return data
-
-
-def calc_selection_threshold(data):
-    V = data.shape[1]
-    selection_threshold = [m * np.var(data[:, j], ddof=1) for j in range(V)]
-    return selection_threshold
 
 
 def calc_stats_i(data_i):
@@ -71,30 +60,6 @@ def calc_stats(data):
     return {'mu': mu,
             'mu_tilde': mu_tilde,
             'sample_var': sample_var}
-
-
-def select_dim(data_i, selection_threshold, mu_hat_i=None):
-    """
-    Select relevant dimensions for clusters, which must obey:
-    s[i][j] ** 2 + (mu[i][j] - mu_tilde[i][j]) ** 2 < s_hat[i][j] ** 2
-    """
-    V = data_i.shape[1]
-    
-    # Return a list of 0/1 values to show whether dim_j is selected.
-    selected_dims = []
-
-    # Calculate relevant statistics.
-    mu_i, mu_tilde_i, sample_var_i = calc_stats_i(data_i)
-
-    # Use medoids if given.
-    if mu_hat_i is not None:
-        mu_tilde_i = mu_hat_i
-
-    # Loop over clusters to find relevant dimensions.
-    for j in range(V):
-        if sample_var_i[j] ** 2 + (mu_i[j] - mu_tilde_i[j]) ** 2 < selection_threshold[j] ** 2:
-            selected_dims.append(j)
-    return selected_dims
 
 
 def bin_cell(data, edges):
@@ -210,105 +175,7 @@ def max_min_dist(data, sd_data, G, selected_dims):
                 # Compare it with current minimum.
                 if up not in up_dist or up_dist[up] > sd_dist:
                     up_dist[up] = sd_dist
-    return max(up_dist.iterkeys(), key=(lambda key: up_dist[key]))
-
-
-def private_seeds_for_labeled_objects(data, labeled_objects_i, labeled_dimensions_i=None):
-    selection_threshold = calc_selection_threshold(data)
-    
-    # Extract i-th cluster's labeled objects.
-    data_i = data[labeled_objects_i]
-    med_i = np.median(data_i, axis=0)
-
-    # Define a candidate set that includes the dimensions selected by SelectDim as well as 
-    # those in labeled_dimensions (if any).
-    candidate_set = select_dim(data_i, selection_threshold, med_i)
-    if labeled_dimensions_i: 
-        candidate_set = list(set(np.concatenate((candidate_set, labeled_dimensions_i))))
-
-    # TODO: Ask Vic to fix score function.
-    # score_ij = score_function_i(i, candidate_set, med_i)
-    score_ij = np.random.rand(len(candidate_set))
-
-    # Each dimension in the set has a probability proportional to 
-    # the score function of being selected as a building dimension of a grid.
-    score_ij /= sum(score_ij)
-    building_dims = np.random.choice(a=candidate_set, size=building_dim_num, replace=False, p=score_ij)
-
-    # Extract the data with building dimensions.
-    data_grid = data[:, building_dims]
-    sd_grid = np.std(data_grid, axis=0)
-    med_i_grid = med_i[building_dims]
-
-    # Apply hill-climbing search to find most dense cell.
-    G_i = hill_climb(data_grid, med_i_grid, sd_grid)
-    return G_i
-
-
-def initialize(data, k, labeled_objects, labeled_dimensions):
-    selection_threshold = calc_selection_threshold(data)
-    sd_data = np.std(data, axis=0)
-    
-    # i-th sample belongs to seed_group_label[i] seed group.
-    seed_group_label = np.zeros(len(data))
-    
-    # Denote the current distance to the closest median.
-    med_dist = np.zeros(len(data))
-    
-    # Private seeds.
-    G = {}
-    
-    # Selected dimensions.
-    selected_dims = {}
-    
-    # Loop over clusters with both labeled objects and labeled dimensions.
-    for i in range(k):
-        if ((i < len(labeled_objects) and len(labeled_objects[i]) > 0) 
-            and (i < len(labeled_dimensions) and len(labeled_dimensions[i]) > 0)):
-            G[i] = private_seeds_for_labeled_objects(data, labeled_objects[i], labeled_dimensions[i])
-            
-            # Find the relevant dimensions for this cluster.
-            selected_dims[i] = select_dim(data[G[i]], selection_threshold)
-    
-    # Loop over clusters with only labeled objects.
-    for i in range(k):
-        if ((i < len(labeled_objects) and len(labeled_objects[i]) > 0) 
-            and not ((i < len(labeled_dimensions) and len(labeled_dimensions[i]) > 0))):
-            G[i] = private_seeds_for_labeled_objects(data, labeled_objects[i])
-            
-            # Find the relevant dimensions for this cluster.
-            selected_dims[i] = select_dim(data[G[i]], selection_threshold)
-        
-    # Loop over clusters with only labeled dimensions.
-    for i in range(k):
-        if ((not (i < len(labeled_objects) and len(labeled_objects[i]) > 0)) 
-            and (i < len(labeled_dimensions) and len(labeled_dimensions[i]) > 0)):
-            candidate_set = labeled_dimensions[i]
-            building_dims = np.random.choice(a=candidate_set, size=building_dim_num, replace=False)
-
-            # Extract the data with building dimensions.
-            data_grid = data[:, building_dims]
-            sd_grid = np.std(data_grid, axis=0)
-
-            # Find absolute peak.
-            G[i] = get_peak(data_grid, sd_grid)
-            
-            # Find the relevant dimensions for this cluster.
-            selected_dims[i] = select_dim(data[G[i]], selection_threshold)
-    
-    # Loop over clusters with no input.
-    for i in range(k):
-        if not ((i < len(labeled_objects) and len(labeled_objects[i]) > 0)
-            and (i < len(labeled_dimensions) and len(labeled_dimensions[i]) > 0)):
-            
-            # Pick the seed whose minimum distance to all the seeds already picked 
-            # by other seed groups is maximum, as the median.
-            med_i = data[max_min_dist(data, sd_data, G, selected_dims)]
-            G[i] = hill_climb(data, med_i, sd_data)
-            
-            # Find the relevant dimensions for this cluster.
-            selected_dims[i] = select_dim(data[G[i]], selection_threshold)
-    return G, selected_dims
+    return max(up_dist.keys(), key=(lambda key: up_dist[key]))
 
 
 def column(matrix, i):
